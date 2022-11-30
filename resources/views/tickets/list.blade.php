811df546f400b5121cf9352f22812e625633a5ae
Roetjan <93587343+Roetjan@users.noreply.github.com>
2022-11-30 11:50:23 +0100

    added a div for text and added styling

--- resources/views/tickets/list.blade.php
+++ resources/views/tickets/list.blade.php
@@ -3,19 +3,18 @@
 @section('content')
    {{-- <a href="{{ route('addEvent') }}">Add event</a> --}}
    <div class="container-fluid mt-10">
-    <div class="row p-2">
-        @foreach ($allTickets as $ticket)
-            <div class="ticketBlock col-6 border border-primary rounded d-flex">
-                <div class="img-fluid w-25">
-                    <img src="https://picsum.photos/150/150" alt="Event_ticket">
+        <div class="row p-2">
+            @foreach ($allTickets as $ticket)
+                <div class="ticketBlock col-8 border border-primary rounded d-flex p-0">
+                    <div class="img-fluid w-30 test-left rounded">
+                        <img src="https://picsum.photos/200/200" alt="Event_ticket">
+                    </div>
+                    <div class="w-70">
+                        <p class="h6">{{ $ticket->user->name }}</p> 
+                            {{ $ticket->user->event_id }}
+                    </div>
                 </div>
-                <div class="text w-75">
-                   <h4>{{ $ticket->user->name }}</h4> 
-                    {{ $ticket->user->qr_hash }}
-                    {{ $ticket->user->event_id }}
-                </div>
-            </div>
-        @endforeach
-    </div>
+            @endforeach
+        </div>
     </div>
 @endsection
