4028b7556b53c2f55c57b3ddbfc6b5da97713487
Roetjan <93587343+Roetjan@users.noreply.github.com>
2022-12-21 20:39:20 +0100

    verwijderd om het beter te maken

--- resources/views/Event/events.blade.php
+++ resources/views/Event/events.blade.php
@@ -4,27 +4,8 @@
 <div class="container-fluid mt-10">
     <div class="Toptext  text-center">
         <p class="h1">Events Lijst</p>
-        <p class="font-weight-light">als je de Events niet goed ziet zet de tab op max</p>
     </div>
     
-    
-    {{-- <div class="row">
-        @foreach ($events as $event)
-            <div class="ticketBlock w-25 border border-primary rounded d-flex p-0">
-                <div class="img-fluid w-25 test-left rounded">
-                    <img src="{{ $event->picture }}" alt="Event_picture">
-                </div>
-                <div class="w-50">
-                    <div class="EventName">
-                        <p class="font-weight-bold">{{ $event->name }}</p> 
-                    </div>
-                    <div class="eventDate">
-                        <p>{{ $event->event_start }}</p>
-                    </div>
-                </div>
-            </div>
-        @endforeach
-    </div> --}}
     <table class="table">
         <thead>
             <th>Event naam</th>
@@ -51,6 +32,7 @@
             @endauth
             @endforeach
         </tbody>
+        @endforeach
     </table>
 </div>
 @endsection
