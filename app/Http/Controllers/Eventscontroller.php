b2fcb7ffa3ee7470ddce6d887f8402de98b52e7a
Roetjan <93587343+Roetjan@users.noreply.github.com>
2022-12-21 20:35:30 +0100

    simlink aangemaakt voor als je een foto wilt toevoegen met de event

--- app/Http/Controllers/Eventscontroller.php
+++ app/Http/Controllers/Eventscontroller.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Event;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class Eventscontroller extends Controller
 {
@@ -28,13 +29,18 @@ class Eventscontroller extends Controller
     public function addEvent(Request $request) {
         $newEvent = new Event();
         $newEvent->name = $request->input('name');
-        $newEvent->picture = $request->input('picture');
         $newEvent->event_start = $request->input('event_start');
         $newEvent->event_end = $request->input('event_end');
         $newEvent->max_tickets = $request->input('max_tickets');
         $newEvent->location = $request->input('location');
         $newEvent->price = $request->input('price');
         $newEvent->description = $request->input('description');
+
+
+        Storage::makeDirectory('public/img');
+        $src = Storage::putfile('public/img', $request->file('picture'));
+        $src = str_replace('public', 'storage', $src);
+        $newEvent->picture = $src;
         $newEvent->save();
 
         return view('/admin/add-event');
