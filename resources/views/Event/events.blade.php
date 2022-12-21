26f1c0f1c13c68d1008a88d6579ddf4c1c6d00b9
Roetjan <93587343+Roetjan@users.noreply.github.com>
2022-12-21 20:40:51 +0100

    een nieuwe lijst en cards aangemaakt voor als je als admin of als gast op de pagina komt

--- resources/views/Event/events.blade.php
+++ resources/views/Event/events.blade.php
@@ -1,6 +1,7 @@
 @extends("layouts.base")
 
 @section('content')
+@auth
 <div class="container-fluid mt-10">
     <div class="Toptext  text-center">
         <p class="h1">Events Lijst</p>
@@ -17,22 +18,41 @@
             @endauth
             
         </thead>
+        @foreach ($events as $event)
         <tbody>
-            @foreach ($events as $event)
+            
             <td>{{ $event->name }}</td> 
             <td>{{ $event->location }}</td>
             <td>{{ $event->created_at }}</td>
-            @auth
                 <td>
                     <a href="{{ Route("deleteEvent", $event->id) }}">Delete</a>
                 </td>
                 <td>
-                    <a href="{{ Route("editEvent", $event->id) }}">Edit</a>
+                    <a href="{{ Route("UpdateEvent", $event->id) }}">Edit</a>
                 </td>
-            @endauth
-            @endforeach
+
         </tbody>
         @endforeach
     </table>
 </div>
+    @endauth
+
+@guest
+    <div class="cards container mt-10" style="width: 18rem">
+        <div class="row">
+        @foreach ($events as $event)
+        
+            <div class="card">
+                <img src="{{asset($event->picture) }}" alt="event_picture" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $event->name }}</h5>
+                    <p class="card-text">{{ $event->description }}</p>
+                    <a href="" class="btn btn-primary">Ticket kopen</a>
+                </div>
+            </div>
+        
+        @endforeach
+        </div>
+    </div>
+@endguest
 @endsection
