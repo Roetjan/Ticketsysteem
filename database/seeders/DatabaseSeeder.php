d3d38b13c02a21004552e0514ed0d8b02c8c8d6d
Roetjan <93587343+Roetjan@users.noreply.github.com>
2023-01-11 12:08:47 +0100

    commented seeders, no use anymore

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([UserSeeder::class]);
-        $this->call([TicketSeeder::class]);
+        // $this->call([UserSeeder::class]);
+        // $this->call([TicketSeeder::class]);
     }
 }
