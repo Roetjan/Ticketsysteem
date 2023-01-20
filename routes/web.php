390428f69bb77193c54c400e7f7b733ada49f8bd
Roetjan <93587343+Roetjan@users.noreply.github.com>
2023-01-20 08:38:34 +0100

    bij de auth van de Routes nog een admin middelware aangemaakt en toegevoegd aan de routes

--- routes/web.php
+++ routes/web.php
@@ -22,20 +22,22 @@ Route::get('/over-ons', [PagesController::class, 'viewAboutUs'])->name('aboutUs'
 
 Route::get('/contact', [PagesController::class, 'viewContact'])->name('contact');
 
-Route::get('/ticket', [TicketsController::class, 'viewTicket'])->name('listTickets')->middleware('auth');
+Route::get('/ticket', [TicketsController::class, 'viewTicket'])->name('listTickets')->middleware('auth', 'admin');
 
 Route::get('/events', [EventsController::class, 'viewEvent']);
 
-Route::get('/admin/edit-event/{eventId}', [Eventscontroller::class, 'showEvent'])->name('showEvent')->middleware('auth');
-Route::post('/admin/edit-event/{eventId}', [Eventscontroller::class, 'UpdateEvent'])->name('UpdateEvent')->middleware('auth');
+Route::get('/admin/edit-event/{eventId}', [Eventscontroller::class, 'showEvent'])->name('showEvent')->middleware('auth', 'admin');
+Route::post('/admin/edit-event/{eventId}', [Eventscontroller::class, 'UpdateEvent'])->name('UpdateEvent')->middleware('auth', 'admin');
 
-Route::get('/admin/delete-event/{eventId}', [Eventscontroller::class, 'deleteEvent'])->middleware('auth')->name("deleteEvent");
+Route::get('/admin/delete-event/{eventId}', [Eventscontroller::class, 'deleteEvent'])->middleware('auth', 'admin')->name("deleteEvent");
 
 Route::post('/create-ticket', [TicketsController::class, 'createTicket'])->name('createTicket');
 
 Route::post('/send-contact', [PagesController::class, 'sendContact'])->name('sendContact');
 
-Route::get('/admin/add-event', [Eventscontroller::class, 'showAddEvent'])->middleware('auth')->name('showEvent');
+Route::get('/admin/add-event', [Eventscontroller::class, 'showAddEvent'])->middleware('auth', 'admin')->name('showEvent');
+
+Route::post('/admin/add-event', [Eventscontroller::class, 'addEvent'])->middleware('auth', 'admin')->name('addEvent');
 
 Route::post('/admin/add-event', [Eventscontroller::class, 'addEvent'])->middleware('auth')->name('addEvent');
 
